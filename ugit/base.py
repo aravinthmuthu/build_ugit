d3615eaea282ff6f76943d8a80576cbcfab8de9d
Aravinth  Muthu <aravinthmuthu@Aravinths-MacBook-Pro.local>
2023-09-15 12:29:39 +0530

    write-tree : hash all files

--- ugit/base.py
+++ ugit/base.py
@@ -7,13 +7,15 @@ def write_tree (directory='.'):
         full_path = os.path.join(directory,item)
         if not is_ignored(full_path):
             if os.path.isfile(full_path):
-                print(full_path)
+                with open(full_path, 'rb') as file:
+                    obj = file.read()
+                    print(data.hash_object(obj), full_path)
             elif os.path.isdir(full_path):
                 write_tree(full_path)
 
 def is_ignored(path):
     # TO-DO add regex support
-    ignore_list = ['.ugit','.git','.gitignore', "__pycache__"]
+    ignore_list = ['.ugit','.git','.gitignore', "__pycache__","ugit.egg-info"]
     ignore = False
     for l in ignore_list:
         if l in path.split('/'):
