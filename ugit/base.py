6eeaf15bb656768f409397d474f71e373154ff51
Aravinth  Muthu <aravinthmuthu@Aravinths-MacBook-Pro.local>
2023-09-15 12:22:12 +0530

    add ignore files

--- ugit/base.py
+++ ugit/base.py
@@ -5,7 +5,17 @@ def write_tree (directory='.'):
     items = os.listdir(directory)
     for item in items:
         full_path = os.path.join(directory,item)
-        if os.path.isfile(full_path):
-            print(full_path)
-        elif os.path.isdir(full_path):
-            write_tree(full_path)
+        if not is_ignored(full_path):
+            if os.path.isfile(full_path):
+                print(full_path)
+            elif os.path.isdir(full_path):
+                write_tree(full_path)
+
+def is_ignored(path):
+    # TO-DO add regex support
+    ignore_list = ['.ugit','.git','.gitignore', "__pycache__"]
+    ignore = False
+    for l in ignore_list:
+        if l in path.split('/'):
+            ignore = True
+    return ignore
